e6fb7031ee2de8e4dea2269b163d68f4ec0bae8f
bijish <bijish_js@outlook.com>
2024-07-03 01:05:45 +0530

    chore: configuring package

---
+++ src/app/api/list-actions/route.ts
@@ -0,0 +1,49 @@
+"use server"
+
+import { NextRequest } from "next/server";
+import * as aggregate from "./aggregator";
+export const POST = async (req: NextRequest) => {
+
+	const body = await req.json();
+	const { functionName, id, args } = body as any;
+
+	try {
+
+		const nameOfFunction = `${functionName}_${id}`;
+		// Access the function in the module
+		const action = aggregate[nameOfFunction];
+
+		// Log the action to debug
+		console.log(`Resolved action:`, action);
+
+		// Ensure the action is a function
+		if (typeof action === 'function') {
+			// Execute the function with the provided arguments
+			let result = null
+			if (args) {
+				result = await action(...args);
+
+			}
+			else {
+				result = await action();
+
+			}
+			console.log('Function result:', result);
+			return new Response(JSON.stringify({ result } || "SUCCESS"), {
+				status: 200,
+			});
+		} else {
+			console.error('Action is not a function:', action);
+			return new Response(JSON.stringify('FAILED'), {
+				status: 400,
+				statusText: 'Action is not a function',
+			});
+		}
+	} catch (error) {
+		console.error('Error loading module:', error);
+		return new Response(JSON.stringify('FAILED'), {
+			status: 400,
+			statusText: error.message,
+		});
+	}
+};
